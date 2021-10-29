ffc10df76ebf3e986beba1b8fb47293b7e5d7a56
Victor Ryabinin <rybvic@gmail.com>
2021-10-29 23:21:50 +0300

    init db script

---
+++ init_rmrk_db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /home/ubuntu
+pg_login="<your_pg_login>"
+pg_pass="<your_pg_password>"
+pg_db_name="rmrk"
+pg_host="localhost"
+pg_connect="postgresql://$pg_login:$pg_pass@$pg_host/$pg_db_name"
+home_dir="/home/ubuntu"
+
+wget https://gateway.pinata.cloud/ipns/precon-lite.rmrk.link -O $home_dir/dump_vLite.dump
+wget https://gateway.pinata.cloud/ipns/precon-mkt.rmrk.app -O $home_dir/dump_v1.dump
+wget https://gateway.pinata.cloud/ipns/precon-rmrk2.rmrk.link -O $home_dir/dump_v2.dump
+
+python3 $home_dir/rmrk2psql.py -i $home_dir/dump_vLite.dump -o $home_dir/output_vLite.sql -v
+python3 $home_dir/rmrk2psql.py -i $home_dir/dump_v1.dump -o $home_dir/output_v1.sql -v
+python3 $home_dir/rmrk2psql.py -i $home_dir/dump_v2.dump -o $home_dir/output_v2.sql -v
+
+psql "$pg_connect" -f $home_dir/output_vLite.sql
+psql "$pg_connect" -f $home_dir/output_v1.sql
+psql "$pg_connect" -f $home_dir/output_v2.sql
